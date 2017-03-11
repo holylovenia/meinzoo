782c87f5a176072c7d8c3df09c42d7e5731c4fcb
Holy Lovenia <holy.lovenia@gmail.com>
2017-03-11 21:34:38 +0700

    Added tiger

---
+++ tiger.h
@@ -0,0 +1,21 @@
+#ifndef TIGER_H
+#define TIGER_H
+
+#include "mammalia.h"
+#include "carnivora.h"
+#include "behavior_tame.h"
+
+class Tiger: public Mammalia, public Carnivora, public BehaviorTame
+{
+	public:
+		Tiger(int _weight);
+		Tiger(const Tiger&);
+		void Interact();
+
+	private:
+		const int defID;
+		const int defRatioMeat;
+		const int defRatioPlant;
+};
+
+#endif
