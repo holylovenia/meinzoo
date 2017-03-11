98bbc90713857f1ee0953ed4afe0b5cd517608d6
Oktavianus Handika <handikaoktavianus29@gmail.com>
2017-03-11 15:45:00 +0700

    added cell.cpp, edited cell.h

--- cell.h
+++ cell.h
@@ -1,23 +1,21 @@
 #ifndef CELL_H
 #define CELL_H
+#include "point.h"
 
 class Cell {
 	public:
 		// ctor
-		Cell(int _x, int _y);
+		Cell(int _x, int _y, bool Accessible);
 		// cctor
 		Cell(Cell& C);
 		// operator=
 		Cell& operator=(Cell& C);
-		// dtor
-		~Cell();
 
 		void setPosition (int _x, int _y);
 		void getPosition (int& _x, int& _y);
 
 	protected:
-		unsigned int x;
-		unsigned int y;
+		Point Location;
 		bool isAccessible;
 
 	private:
