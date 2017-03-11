eb88e29673dfe911a81979bd5004d85c8ce5c292
Holy Lovenia <holy.lovenia@gmail.com>
2017-03-11 14:51:13 +0700

    Edited animal_diet.h

--- animal_diet.h
+++ animal_diet.h
@@ -3,21 +3,12 @@
 
 class AnimalDiet {
 	public:
-		// ctor
-		AnimalDiet();
-		// cctor
-		AnimalDiet(AnimalDiet& A);
-		// operator=
-		AnimalDiet& operator=(AnimalDiet& A);
-		// dtor
-		~AnimalDiet();
-
 		bool CanEatMeat();
 		bool CanEatPlants();
 
 	protected:
 		bool eatMeat;
 		bool eatPlants;
-}
+};
 
 #endif
