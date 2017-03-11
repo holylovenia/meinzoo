aa59b2f2f90532e5dff55f397a7114e119de839b
Holy Lovenia <holy.lovenia@gmail.com>
2017-03-11 15:08:41 +0700

    Edited habitat.h

--- habitat.h
+++ habitat.h
@@ -3,24 +3,9 @@
 
 #include "cell.h"
 
-class Habitat : public Cell {
-	public:
-		// ctor
-		Habitat();
-		// ctor with parameter
-		Habitat(char type);
-		// cctor
-		Habitat(const Habitat& H);
-		// operator=
-		Habitat& operator=(Habitat& H);
-		// dtor
-		~Habitat();
-
+class Habitat: public Cell {
 	protected:
 		char type;
-
-	private:
-		const char defType = 'L';
 }
 
 #endif
