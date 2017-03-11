26e9bcd689c7c67384cd4863838c7b9b2ddf7816
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2017-03-11 22:30:26 +0700

    Fixed indentation and point.h

--- point.cpp
+++ point.cpp
@@ -2,30 +2,30 @@
 
 // Setter
 void Point::setX(int _x) {
-    x = _x;
+	x = _x;
 }
 void Point::setY(int _y) {
-    y = _y;
+	y = _y;
 }
 
 // Getter
 int Point::getX() {
-    return x;
+	return x;
 }
 int Point::getY() {
-    return y;
+	return y;
 }
 
 // Movement
 void Point::moveUp() {
-    y += 1;
+	y += 1;
 }
 void Point::moveRight() {
-    x += 1;
+	x += 1;
 }
 void Point::moveDown() {
-    y -= 1;
+	y -= 1;
 }
 void Point::moveLeft() {
-    x -= 1;
+	x -= 1;
 }
--- point.h
+++ point.h
@@ -1,25 +1,21 @@
+// File: point.h
+
 #ifndef POINT_H
 #define POINT_H
 
 class Point {
-    public:
-        // Setter
-        void setX(int _x);
-        void setY(int _y);
+	public:
+		// Setter
+		void setX(int _x);
+		void setY(int _y);
 
-        // Getter
-        int getX();
-        int getY();
+		// Getter
+		int getX();
+		int getY();
 
-        // Movement
-        void moveUp();
-        void moveRight();
-        void moveDown();
-        void moveLeft();
-
-    private:
-        unsigned int x;
-        unsigned int y;
+	private:
+		unsigned int x;
+		unsigned int y;
 };
 
 #endif
