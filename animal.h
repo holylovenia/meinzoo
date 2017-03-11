c2e228e8894047ce9cc129ffffc941d618fe558a
Holy Lovenia <holy.lovenia@gmail.com>
2017-03-11 18:37:13 +0700

    Edited animal.h and added animal_behavior.h

--- animal.h
+++ animal.h
@@ -2,28 +2,16 @@
 #define ANIMAL_H
 
 #include <string>
+#include <set>
 
 class Animal {
 	public:
-		// ctor
-		Animal();
-		// ctor with parameter
-		Animal(bool _wild);
-		// cctor
-		Animal(const Animal& A);
-		// operator=
-		Animal& operator=(Animal& A);
-		// dtor
-		~Animal();
 		// others
 		virtual void Interact() = 0;
 		virtual void Move() = 0;
-		bool IsWild();
-
 
 	protected:
 		int ID;
-		bool wild;
 		int limbCount;
 		int weight;
 		std::string skinType;
---
+++ animal_behavior.h
@@ -0,0 +1,12 @@
+#ifndef ANIMAL_BEHAVIOR_H
+#define ANIMAL_BEHAVIOR_H
+
+class AnimalBehavior {
+	public:
+		bool getBehavior();
+
+	protected:
+		bool wild;
+}
+
+#endif
