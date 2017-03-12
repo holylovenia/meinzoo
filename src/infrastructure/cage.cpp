8a279cfc6c9deebd57b7c54188a0c893f37ca8fd
Felix Limanta <13515065@std.stei.itb.ac.id>
2017-03-12 14:24:54 +0700

    Cage dibenerin

--- src/infrastructure/cage.cpp
+++ src/infrastructure/cage.cpp
@@ -1,9 +1,2 @@
 #include "cage.h"
 
-void AddPoint(const Point& p) {
-	area.insert(p);
-}
-
-void RemovePoint(const Point& p) {
-	area.erase(p);
-}
--- src/infrastructure/cage.h
+++ src/infrastructure/cage.h
@@ -6,17 +6,25 @@
 #include <set>
 #include "../misc/point.h"
 
+using namespace std;
+
 class Cage {
 	public:
 		// tidak perlu ctor dan dtor karena bitwise copy tidak apa-apa
 		
-		void AddPoint(const Point& p);
-		
-		void RemovePoint(const Point& p);
+		void AddPoint(Point& p);
+		void RemovePoint(Point& p);
 		
 	private:
 		set<Point> area;
 };
 
+void Cage::AddPoint(Point& p) {
+	area.insert(p);
+}
+
+void Cage::RemovePoint(Point& p) {
+	//area.erase(p);
+}
 
 #endif
