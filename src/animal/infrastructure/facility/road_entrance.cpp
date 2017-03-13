8ba096c887889146601730fcdede1abfe6c781f9
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2017-03-13 19:06:05 +0700

    Fixed fuckups

--- src/animal/infrastructure/facility/road_entrance.cpp
+++
@@ -1,6 +0,0 @@
-#include "road_entrance.h"
-
-
-RoadEntrance::RoadEntrance(bool Accessible): Road(Accessible), defRoadEntrance(true) {
-	isEntrance = defRoadEntrance;
-}
--- src/animal/infrastructure/facility/road_exit.cpp
+++
@@ -1,6 +0,0 @@
-#include "road_exit.h"
-
-
-RoadExit::RoadExit(bool Accessible): Road(Accessible), defRoadExit(true) {
-	isExit = defRoadExit;
-}
